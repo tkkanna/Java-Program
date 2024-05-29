4e6399349cd22b3bd54cf03eee8322d478fbac32
tkkanna <thamaraikannantk53@gmail.com>
2024-05-29 12:08:06 +0530

    Add files via upload

---
+++ L4p2.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class L4p2
+{
+public static void main (String[] args)
+{
+Animal animal1 = new Cat();
+Animal animal2 = new Dog();
+
+
+animal1.eat();
+animal1.behaviour();
+
+
+animal2.eat();
+animal2.behaviour();
+}
+}
