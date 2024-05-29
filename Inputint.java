82b44537c3f7e48abad8b03f118be2122cd73ce8
tkkanna <thamaraikannantk53@gmail.com>
2024-05-29 12:03:16 +0530

    Add files via upload

---
+++ Inputint.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class Inputint
+{
+public static void main(String arg[])
+{
+Scanner obj=new Scanner(System.in);
+System.out.println("Enter roll no");
+int rollno=obj.nextInt();
+System.out.println("rollno is "+rollno);
+}
+} 
